ed777374a27d311119776dba8b6605ca4d28a830
dinesh19aug@gmail.com <dinesh19aug@gmail.com>
2020-04-11 22:38:30 -0400

    Added example for running task in thread pool.

---
+++ src/main/java/com/dinesh/concurrent/chain/thread/User.java
@@ -0,0 +1,16 @@
+package com.dinesh.concurrent.chain.thread;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User
+{
+    private long id;
+
+}
