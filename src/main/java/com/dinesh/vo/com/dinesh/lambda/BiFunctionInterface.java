9e0ffc6b16ca43ea0535a03522f0cc921ff2298d
dinesh19aug@gmail.com <dinesh19aug@gmail.com>
2020-04-11 20:26:14 -0400

    Added example for BiFunction

---
+++ src/main/java/com/dinesh/vo/com/dinesh/lambda/BiFunctionInterface.java
@@ -0,0 +1,20 @@
+package com.dinesh.vo.com.dinesh.lambda;
+
+import java.util.function.BiFunction;
+
+public class BiFunctionInterface
+{
+    //biFunction<T,U,R> T and U are params and R is the result
+    static BiFunction<Integer, Integer, String> addTwoString =(x,y)->{
+        String result;
+        Integer sum = x+y;
+        result = x + " + " + y + " = " + sum;
+        return result;
+    };
+    public static void main(String[] args)
+    {
+        int a = 3;
+        int b = 4;
+        System.out.println(addTwoString.apply(a,b));
+    }
+}
